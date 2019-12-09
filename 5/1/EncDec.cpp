53dfdcc2829fe9ae53a818f7f8afb5c27170532d
Vliseev <vliseev.92@mail.ru>
2019-12-09 21:28:33 +0300

    hw 5 is done

--- 5/1/EncDec.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by vlad on 01.12.2019.
-//
-
-#include "EncDec.h"
-void Encode(IInputStream& original, IOutputStream& compressed) {
-}
-void Decode(IInputStream& compressed, IOutputStream& original) {
-}
--- 5/1/EncDec.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by vlad on 01.12.2019.
-//
-#pragma once
-#include "Stream.h"
-// Метод архивирует данные из потока original
-void Encode(IInputStream& original, IOutputStream& compressed);
-// Метод восстанавливает оригинальные данные
-void Decode(IInputStream& compressed, IOutputStream& original);
