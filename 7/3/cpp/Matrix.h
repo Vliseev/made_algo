c6a22f8dccd6acd873022e2647100e23c2d49f53
Vliseev <Vliseev.92@mail.ru>
2019-12-27 13:34:43 +0300

    in progress

--- 7/3/cpp/Matrix.h
+++ 7/3/cpp/Matrix.h
@@ -1,5 +1,103 @@
-//
-// Created by vlad on 26.12.2019.
-//
-
 #pragma once
+#include <algorithm>
+#include <cstddef>
+#include <exception>
+#include <stdexcept>
+
+template <class T>
+class Matrix {
+    size_t n_row_;
+    size_t n_col_;
+    T *data_ = nullptr;
+
+   public:
+    Matrix(size_t n_row, size_t n_col);
+    ~Matrix();
+    size_t GetNRows() const {
+        return n_row_;
+    };
+    size_t GetColumns() const {
+        return n_col_;
+    };
+
+    T *operator()(size_t i);
+    T operator()(size_t i, size_t j);
+    T &operator()(size_t i, size_t j) const;
+
+    bool operator==(const Matrix &rhs) const;
+    bool operator!=(const Matrix &rhs) const;
+    Matrix(const Matrix &);
+    Matrix(Matrix &&) noexcept;
+
+    Matrix &operator=(const Matrix &);
+    Matrix &operator=(Matrix &&) noexcept;
+};
+template <class T>
+Matrix<T>::Matrix(size_t n_row, size_t n_col)
+    : n_row_(n_row), n_col_(n_col), data_(new T[n_col * n_row]) {
+}
+template <class T>
+Matrix<T>::~Matrix() {
+    delete[] data_;
+}
+template <class T>
+bool Matrix<T>::operator==(const Matrix &rhs) const {
+    return n_row_ == rhs.n_row_ && n_col_ == rhs.n_col_ &&
+           std::equal(data_, data_ + n_row_ * n_col_, rhs.data_);
+}
+template <class T>
+bool Matrix<T>::operator!=(const Matrix &rhs) const {
+    return !this == rhs;
+}
+template <class T>
+T *Matrix<T>::operator()(size_t i) {
+    if (i >= n_row_)
+        throw std::out_of_range("invalid row");
+    return data_ + i * n_col_ * sizeof(T);
+}
+template <class T>
+T Matrix<T>::operator()(size_t i, size_t j) {
+    if (i >= n_row_ || j >= n_col_)
+        throw std::out_of_range("invalid idx");
+    return (data_ + i * n_col_ + j) * sizeof(T);
+}
+template <class T>
+T &Matrix<T>::operator()(size_t i, size_t j) const {
+    if (i >= n_row_ || j >= n_col_)
+        throw std::out_of_range("invalid idx");
+    return (data_ + i * n_col_ + j) * sizeof(T);
+}
+template <class T>
+Matrix<T>::Matrix(const Matrix &other)
+    : n_col_(other.n_col_),
+      n_row_(other.n_row_),
+      data_(new T[other.n_col_ * other.n_row_]) {
+    std::copy(other.data_, other.data_ + n_row_ * n_col_, data_);
+}
+template <class T>
+Matrix<T> &Matrix<T>::operator=(const Matrix &other) {
+    if (std::addressof(other) == this)
+        return *this;
+    n_col_ = other.n_col_;
+    n_row_ = other.n_row_;
+    data_ = new T[other.n_col_ * other.n_row_];
+    std::copy(other.data_, other.data_ + n_row_ * n_col_, data_);
+    return *this;
+}
+template <class T>
+Matrix<T>::Matrix(Matrix &&other) noexcept
+    : n_col_(other.n_col_), n_row_(other.n_row_) {
+    data_ = other.data_;
+    other.data_ = nullptr;
+}
+template <class T>
+Matrix<T> &Matrix<T>::operator=(Matrix &&other) noexcept {
+    if (std::addressof(other) == this)
+        return *this;
+
+    n_col_ = other.n_col_;
+    n_row_ = other.n_row_;
+    data_ = other.data_;
+    other.data_ = nullptr;
+    return *this;
+}
--- 7/3/cpp/main.cpp
+++ 7/3/cpp/main.cpp
@@ -1,4 +1,112 @@
+
+#include <algorithm>
 #include <iostream>
+#include <vector>
+#include "Matrix.h"
+
+class UnFind {
+    std::vector<int> ids_;
+    std::vector<int> rank_;
+
+    int Find(int x);
+
+   public:
+    explicit UnFind(int n);
+    bool Find(int x, int y);
+    void Union(int x, int y);
+};
+
+UnFind::UnFind(int n) {
+    ids_.resize(n);
+    rank_.resize(n);
+    for (int i = 0; i < n; ++i) {
+        ids_[i] = i;
+        rank_[i] = 1;
+    }
+}
+
+int UnFind::Find(int x) {
+    std::vector<int> path{x};
+    int root = ids_[x];
+
+    while (root != path.back()) {
+        path.push_back(root);
+        root = ids_[root];
+    }
+
+    for (auto el : path)
+        ids_[el] = root;
+
+    return root;
+}
+
+bool UnFind::Find(int x, int y) {
+    return Find(x) == Find(y);
+}
+
+void UnFind::Union(int x, int y) {
+    int i = Find(x);
+    int j = Find(y);
+    if (i == j)
+        return;
+    if (rank_[i] < rank_[j]) {
+        ids_[i] = j;
+        rank_[j] += rank_[i];
+    } else {
+        ids_[j] = i;
+        rank_[i] += rank_[j];
+    }
+}
+
+struct Graph {
+   public:
+    Matrix<double> matrix_;
+    int n_vertex_;
+
+    explicit Graph(int n) : n_vertex_(n), matrix_(n, n){};
+    Graph(const Graph& other) = default;
+    Graph(Graph&& other) noexcept = default;
+    Graph& operator=(const Graph& other) = default;
+    Graph& operator=(Graph&& other) noexcept = default;
+
+    void AddEdge(int u, int v, int w);
+    void SortEdges();
+};
+
+void Graph::AddEdge(int u, int v, int w) {
+    edges_.emplace_back(u, v, w);
+}
+void Graph::SortEdges() {
+    std::sort(std::begin(edges_), std::end(edges_),
+              [](const Edge& e1, const Edge& e2) { return e1.w_ < e2.w_; });
+}
+
+struct MstHelper {
+    const Graph& g_;
+    int min_w_;
+
+    explicit MstHelper(const Graph& graph) : g_(graph), min_w_(0){};
+    void FindMst();
+};
+void MstHelper::FindMst() {
+    UnFind uf(g_.n_vertex_);
+
+    for (int i = 0, v = 1; i < g_.edges_.size() && v < g_.n_vertex_; ++i) {
+        if (!uf.Find(g_.edges_[i].u_, g_.edges_[i].v_)) {
+            uf.Union(g_.edges_[i].u_, g_.edges_[i].v_);
+            min_w_ += g_.edges_[i].w_;
+            v++;
+        }
+    }
+}
+
+int WeightMst(Graph& g) {
+    g.SortEdges();
+    MstHelper mst(g);
+    mst.FindMst();
+
+    return mst.min_w_;
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
